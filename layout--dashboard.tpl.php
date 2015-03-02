3214a868a18c49a067cc2cb62d4ddf21b127a180
Peter Anderson <peter@panda.id.au>
2015-03-03 10:26:34 +1100

    Initial commit.

---
+++ layout--dashboard.tpl.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @file
+ * Template for the Dashboard layout.
+ *
+ * Variables:
+ * - $title: The page title, for use in the actual HTML content.
+ * - $messages: Status and error messages. Should be displayed prominently.
+ * - $tabs: Tabs linking to any sub-pages beneath the current page
+ *   (e.g., the view and edit tabs when displaying a node).
+ * - $action_links: Array of actions local to the page, such as 'Add menu' on
+ *   the menu administration interface.
+ * - $classes: Array of CSS classes to be added to the layout wrapper.
+ * - $attributes: Array of additional HTML attributes to be added to the layout
+ *     wrapper. Flatten using backdrop_attributes().
+ * - $content: An array of content, each item in the array is keyed to one
+ *   region of the layout. This layout supports the following sections:
+ *   - $content['header']
+ *   - $content['sidebar']
+ *   - $content['content']
+ *   - $content['footer']
+ */
+?>
+
+<div class="layout--dashboard <?php print implode(' ', $classes); ?>"<?php print backdrop_attributes($attributes); ?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+
+  <?php if ($content['header']): ?>
+    <header class="l-header"><?php print $content['header']; ?></header>
+  <?php endif; ?>
+
+  <?php if ($messages): ?>
+    <section class="l-messages"><?php print $messages; ?></section>
+  <?php endif; ?>
+
+  <div class="l-container">
+    <main class="l-content" id="main-content">
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+
+      <?php if ($tabs): ?>
+        <div class="tabs"><?php print $tabs; ?></div>
+      <?php endif; ?>
+
+      <?php print $action_links; ?>
+      <?php print $content['content']; ?>
+    </main>
+
+    <?php if ($content['sidebar']): ?>
+      <div class="l-sidebar"><?php print $content['sidebar']; ?></div>
+    <?php endif; ?>
+  </div>
+
+  <?php if ($content['footer']): ?>
+    <div class="l-footer"><?php print $content['footer']; ?></div>
+  <?php endif; ?>
+</div>
+
